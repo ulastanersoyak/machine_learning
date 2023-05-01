66c19dadefae4d7089c88be2c337f7955c83ecc9
Ulaş Tan <92662728+UlasTanErsoyak@users.noreply.github.com>
2023-05-01 13:43:43 +0300

    added predict

--- other_machine_learning_experiments/mnist_digit_classification/main.py
+++ other_machine_learning_experiments/mnist_digit_classification/main.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader 
 from torchvision import datasets
 from torchvision.transforms import ToTensor
+import time
 
 BATCH_SIZE = 128
 EPOCHS = 10
@@ -53,6 +54,8 @@ def create_dataloader(train_data,test_data):
 
 def set_device():
     if torch.cuda.is_available():
+        print(torch.cuda.device(0))
+        print(torch.cuda.get_device_name(0))
         return "cuda"
     return "cpu"
 
@@ -71,25 +74,48 @@ def train_one_epoch(model,train_dataloader,loss_fn,optimizer,device):
 
 def train(model,train_dataloader,loss_fn,optimizer,device,epochs):
     for i in range(epochs):
+        start = time.time()
         print(f"epoch = {i+1}")
         train_one_epoch(model,train_dataloader,loss_fn,optimizer,device)
+        end = time.time()
+        print(f"train epoch {i+1} took {end-start} seconds")
         print("----------------------------------------------------------")
     print("DONE TRAINING!")
 
 
+def load_model():
+    model = Model()
+    state_dict = torch.load("model.pth")
+    model.load_state_dict(state_dict)
+    return model
+
+def predict(model,input,target,class_mapping):
+    model.eval()
+    with torch.no_grad():
+        predictions = model(input)
+        index = predictions[0].argmax()
+        predicted = class_mapping[index]
+        excepted = class_mapping[target]
+    return predicted,excepted
+
 if __name__ == "__main__":
-    train_data,test_data=download_dataset() 
-    train_dataloader,test_dataloader = create_dataloader(train_data,test_data)
-    device = set_device()
-    print(f"using device = {device}")
-    model = Model().to(device)
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(),LEARNING_RATE)
-    train(model,train_dataloader,loss_fn,optimizer,device,EPOCHS)
-    torch.save(model.state_dict(),"model.pth")
-    print("model saved!")
+    x = input()
+    if(x == "1"):
+        train_data,test_data=download_dataset() 
+        train_dataloader,test_dataloader = create_dataloader(train_data,test_data)
+        device = set_device()
+        print(f"using device = {device}")
+        model = Model().to(device)
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(),LEARNING_RATE)
+        train(model,train_dataloader,loss_fn,optimizer,device,EPOCHS)
+        torch.save(model.state_dict(),"model.pth")
+        print("model saved!")
 
-
-
-
-
+    else:
+        class_mapping = ["0","1","2","3","4","5","6","7","8","9"]
+        _,test_data=download_dataset() 
+        input,target = test_data[0][0],test_data[0][1]
+        model = load_model()
+        predicted,excepted = predict(model,input,target,class_mapping)
+        print(f"predicted -> {predicted} || expected -> {excepted}")
