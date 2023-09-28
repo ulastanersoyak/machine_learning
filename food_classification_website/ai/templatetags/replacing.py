ab541aa705c2c77b483b0a60cc841046dc741095
ulastanersoyak <ulastanersoyak@hotmail.com>
2023-09-28 13:15:56 +0300

    formatfix

--- food_classification_website/ai/templatetags/replacing.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def replacing(value): #Custom tag use for replacing '_' with blank to format our class names to visualize better
-    return value.replace("_"," ")
